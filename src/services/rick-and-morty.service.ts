16e5d2ae8be5ab266b85677301ab6dae14d20367
stevenzuluaga <steven.zuluaga@wompi.com>
2025-10-15 18:03:59 -0500

    Update API URL in .env and fix character endpoint in rick-and-morty.service.ts

--- src/services/rick-and-morty.service.ts
+++ src/services/rick-and-morty.service.ts
@@ -5,7 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_RICK_AND_MORTY_URL_API;
 
 export const getCharacters = async (queryString?: string, page: number = 1) => {
   const result = await clientHttp.get<CharacterPagination>(
-    `${API_BASE_URL}/character?name=${queryString}&page=${page}&limit=${5}`
+    `${API_BASE_URL}/characters?name=${queryString}&page=${page}&count=${5}`
   );
 
   //console.table(result.data.results);
